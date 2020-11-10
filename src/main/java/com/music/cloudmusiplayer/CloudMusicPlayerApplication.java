11357fed636a8fdbcc20cbeb0b426d4695cc49b0
PPeony <1916008936@qq.com>
2020-11-10 09:33:17 +0800

    initial

---
+++ src/main/java/com/music/cloudmusiplayer/CloudMusicPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.music.cloudmusiplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMusicPlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMusicPlayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/music/cloudmusiplayer/CloudMusicPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.cloudmusiplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMusicPlayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
